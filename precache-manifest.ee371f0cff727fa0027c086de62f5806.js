41d77d1cf9f1afc46375035ecb51923b6cc3d392
Bruno Guo <brunoguo@usp.br>
2020-07-08 22:00:06 -0300

    Updates

---
+++ precache-manifest.ee371f0cff727fa0027c086de62f5806.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcdeef838045e27d42a430a5aeb6019",
+    "url": "/pcs-3443/index.html"
+  },
+  {
+    "revision": "d7f2c0135781449ae19c",
+    "url": "/pcs-3443/static/js/2.0d564b5b.chunk.js"
+  },
+  {
+    "revision": "64852569161b7cf61c9a91245eefd384",
+    "url": "/pcs-3443/static/js/2.0d564b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6349d1a3766ee5459f19",
+    "url": "/pcs-3443/static/js/main.7b68c3d5.chunk.js"
+  },
+  {
+    "revision": "46694f9afdf0da933afd",
+    "url": "/pcs-3443/static/js/runtime-main.7f23848e.js"
+  },
+  {
+    "revision": "0992738efa78ce6bea92888e3ed1bbf2",
+    "url": "/pcs-3443/static/media/logo.0992738e.png"
+  },
+  {
+    "revision": "5a2b06593ab02db81d4e3f2145c03d84",
+    "url": "/pcs-3443/static/media/logo_white.5a2b0659.png"
+  }
+]);
